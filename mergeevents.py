7952cfbb918845d4bf80d2bb3be9b4b5253e69ee
Andrew Houghton <houghtonandrew0@gmail.com>
2018-09-07 06:46:15 +1000

    Stripped unused fields

--- mergeevents.py
+++ mergeevents.py
@@ -1,10 +1,21 @@
 import scrape
 import showevents
+from pprint import pprint
+
+
+def strip_event_details(calendar_event):
+    return {
+        'end': calendar_event['end'],
+        'start': calendar_event['start'],
+        'summary': calendar_event['summary'],
+        'id': calendar_event['id'],
+    }
 
 
 def merge(calendar_events, website_events):
-    print(calendar_events)
-    print(website_events)
+    pprint(calendar_events)
+    calendar_events = list(map(strip_event_details, calendar_events))
+    pprint(calendar_events)
 
 if __name__ == '__main__':
     merge(showevents.main(), scrape.main())
