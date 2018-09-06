ac4992b11853c95772eff8bb8d4e6443f8b365d8
Andrew Houghton <andrew.houghton@cba.com.au>
2018-09-06 13:57:48 +1000

    found events table

--- scrape.py
+++ scrape.py
@@ -1,11 +1,11 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
 # SCRAPING_URL = "http://www.penrithwhitewater.com.au/water-times-and-events"
 # html_doc = requests.get(SCRAPING_URL).text
 with open('content.html', 'r') as handle:
     html_doc = handle.read()
 
-print(html_doc)
 soup = BeautifulSoup(html_doc, 'html.parser')
-print(soup)
+table = soup.find_all("div", class_="events-table")[0]
+print(table)
