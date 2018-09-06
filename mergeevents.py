d4374ec1d2b0256b6af88db2a4d537ef3e2da45a
Andrew Houghton <houghtonandrew0@gmail.com>
2018-09-07 08:01:31 +1000

    update logic finished

--- mergeevents.py
+++ mergeevents.py
@@ -4,6 +4,10 @@ from pprint import pprint
 from datetime import datetime
 
 
+def events_match(c_event, w_event):
+    return w_event['start'] == c_event['start'] and w_event['end'] == c_event['end']
+
+
 def to_bloody_datetime(date):
     if ":" == date[-3:-2]:
         date = date[:-3] + date[-2:]
@@ -26,23 +30,47 @@ def flatten_website_sessions(website_events):
     return outlist
 
 
+def set_matched_status(array, status):
+    for i in range(len(array)):
+        array[i]['match'] = status
+    return array
+
+
+def mark_pairs_matched(calendar_events, website_events):
+    # Find matching events
+    for c_ind in range(len(calendar_events)):
+        c_event = calendar_events[c_ind]
+        if not c_event['match']:
+            for w_ind in range(len(website_events)):
+                w_event = website_events[w_ind]
+                if not website_events[w_ind]['match']:
+                    if events_match(c_event, website_events[w_ind]):
+                        # These events match
+                        calendar_events[c_ind]['match'] = True
+                        website_events[w_ind]['match'] = True
+
+                if website_events[w_ind]['match']:
+                    break
+        if calendar_events[c_ind]['match']:
+            break
+    return (calendar_events, website_events)
+
+
 def merge(calendar_events, website_events):
     calendar_events = list(map(strip_event_details, calendar_events))
     website_events = flatten_website_sessions(website_events)
 
-    # For each item in the website look for it on the calendar.
-    for c_ind in range(len(calendar_events)):
-        for w_ind in range(len(website_events)):
-            c_event = calendar_events[c_ind]
-            w_event = website_events[w_ind]
+    calendar_events = set_matched_status(calendar_events, False)
+    website_events = set_matched_status(website_events, False)
 
-            pprint(c_event)
-            print('')
-            pprint(w_event)
-            print('')
-            break
-        break
-    # If it exists delete both copies
+    calendar_events, website_events = mark_pairs_matched(calendar_events, website_events)
+
+    # Delete unmatched calendar events
+    calendar_delete_ids = [x['id'] for x in calendar_events if not x['match']]
+
+    # Insert new calendar events
+    calendar_new_entries = [x for x in website_events if not x['match']]
+    pprint(calendar_new_entries)
 
 if __name__ == '__main__':
     merge(showevents.main(), scrape.main())
